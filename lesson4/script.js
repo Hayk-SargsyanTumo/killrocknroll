f6414dec3bb994ea6ca436b6102ffbb55508787c
hayk-tumo <hayk.khachatryan10.y@tumo.org>
2023-02-19 16:09:30 +0400

    chatbot

---
+++ lesson4/script.js
@@ -0,0 +1,11 @@
+var p = document.querySelectorAll('p')
+console.log(p);
+var a = document.querySelectorAll('.a')
+console.log(a)
+var id = document.querySelector('#myP')
+console.log(id)
+
+id.addEventListener('click',()=>{
+console.log('click')
+id.style.color = 'red';
+})
